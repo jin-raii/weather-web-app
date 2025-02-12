b3ef0e50210af2c0cd5f9c706470e3b85d3c24b2
jin-raii <jinrai577@gmail.com>
2025-02-12 07:42:27 +0545

    Feat: implemented static UI

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from weather_ui.views import test
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', test)
+    path('', include('weather_ui.urls'))
 ]
--- weather_ui/views.py
+++ weather_ui/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import request, HttpResponse
 # Create your views here.
 def test(request):
-    return HttpResponse('hello world')
+    return render(request, 'index.html', context={'hello':'there'})
